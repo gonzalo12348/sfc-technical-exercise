68648715ae8ec041ee476c75c9aff9de32d5aac1
gonzalo12348 <gonzaloarancibia48@gmail.com>
2023-07-24 11:44:23 +0200

    feat(odourObservatory): :sparkles: Add odourObservatory page

---
+++ src/constants/const-slider-options.js
@@ -0,0 +1,17 @@
+// Opciones para el slider de Intensidad
+export const intensidadMarks = [
+  { value: 1, label: "Muy Débil" },
+  { value: 2, label: "Débil" },
+  { value: 3, label: "Normal" },
+  { value: 4, label: "Fuerte" },
+  { value: 5, label: "Muy Fuerte" },
+];
+
+// Opciones para el slider de Agradabilidad
+export const agradabilidadMarks = [
+  { value: 1, label: "Extremadamente Agradable" },
+  { value: 2, label: "Agradable" },
+  { value: 3, label: "Normal" },
+  { value: 4, label: "Desagradable" },
+  { value: 5, label: "Extremadamente Desagradable" },
+];
